99d82cb6c383ced57e812d6378046df7caee634a
guille-flores <memo.rfl97@gmail.com>
2022-04-04 23:31:54 -0500

    2022-04-04: Adding classes, arrays for products, functions and dates

--- javascript/index.js
+++ javascript/index.js
@@ -6,22 +6,50 @@ if(login){
     alert("¡Hola!\n\nGracias por escoger Simple Nature, a continuación podrás seleccionar los productos que desees agregar al carrito.");
 }
 
+const carrito = [];
+const hoy = new Date();
+const arriving = new Date();
+arriving.setDate(arriving.getDate()+7);
 
 //Definiendo las clases que son los productos
-const chia = {nombre: "Chia", price: 50, available: true};
-const lenteja = {nombre: "Lenteja Criolla", price: 70, available: false};
-const frijol = {nombre: "Frijol", price: 30, available: true};
-const arroz = {nombre: "Arroz Blanco", price: 20, available: true};
-const arrozint = {nombre: "Arroz Integral", price: 25, available: false};
-const bebidacoco = {nombre: "Bebida de Coco", price: 25, available: true};
-const bebidasoya = {nombre: "Bebida de Soya", price: 25, available: true};
-const lecheent = {nombre: "Leche Entera", price: 15, available: true};
-const lechelight = {nombre: "Leche Light", price: 18, available: true};
-const chocolate = {nombre: "Chocolate", price: 18, available: true};
-const nueces = {nombre: "Nuez de la India", price: 80, available: false};
+class NewProduct{
+    constructor(product, price, id, stock){
+        this.product = product.charAt(0).toUpperCase() + product.slice(1).toLowerCase(); //To capitalize first letter only
+        this.price = price;
+        this.id = id;
+        this.stock = stock;
+    }
 
+    addOrder(){
+        if (this.stock > 0){
+            if(confirm(`Este producto tiene un costo de \$${this.price} MXN, ¿seguro que desea agregarlo al carrito?`)){
+                this.stock = this.stock - 1;
+                if (!carrito.includes(this.product)){
+                    carrito.push(this.product);
+                }
+                return parseFloat(this.price);
+            }else{
+                return 0;
+            }
+        }else{
+            alert(`Lo sentimos, por el momento no contamos con ${this.product}.`);
+            return 0;
+        }
+    }
+}
 
-
+const productos = [];
+productos.push(new NewProduct("ChIA", 50, 123212441, 5));
+productos.push(new NewProduct("lenteja criolla", 50, 12323241, 0));
+productos.push(new NewProduct("Frijol", 30, 2131241, 2));
+productos.push(new NewProduct("Arroz Blanco", 20, 213415, 1));
+productos.push(new NewProduct("Arroz Integral", 25, 21324, 0));
+productos.push(new NewProduct("Bebida de Coco", 25,416663, 4));
+productos.push(new NewProduct("Bebida de Soya", 25, 1414244, 10));
+productos.push(new NewProduct("Leche Entera", 15, 54325, 8));
+productos.push(new NewProduct("Leche Light", 18, 4433434, 9));
+productos.push(new NewProduct( "Chocolate", 18, 55425, 2));
+productos.push(new NewProduct("Nuez de la India", 80, 21321, 0));
 
 var finalizar = false; //Para terminar las compras e ir a pagar
 var cancelled = false; //en caso de que el usuario seleccione cancelar
@@ -35,13 +63,13 @@ while(!finalizar){
                 var producto = prompt("Ingrese solamente el número del producto a a elegir:\n\n1. Chía\n\n2. Lentejas\n\n3. Frijol"); 
                 switch(producto){
                     case "1":
-                        total = total + agregarCarrito(chia);
+                        total = total + agregarCarrito("chia");
                         break;
                     case "2":
-                        total = total + agregarCarrito(lenteja);
+                        total = total + agregarCarrito("lenteja criolla");
                         break;
                     case "3":
-                        total = total + agregarCarrito(frijol);
+                        total = total + agregarCarrito("frijol");
                         break;
                     default:
                         alert("Opción inválida, por favor ingrese solo el número de la opción deseada del menú.");
@@ -51,10 +79,10 @@ while(!finalizar){
                 producto = prompt("Ingrese solamente el número del producto a a elegir:\n\n1. Arroz Blanco\n\n2. Arroz Integral");
                 switch(producto){
                     case "1":
-                        total = total + agregarCarrito(arroz);
+                        total = total + agregarCarrito("arroz blanco");
                         break;
                     case "2":
-                        total = total + agregarCarrito(arrozint);
+                        total = total + agregarCarrito("arroz integral");
                         break;
                     default:
                         alert("Opción inválida, por favor ingrese solo el número de la opción deseada del menú.");
@@ -64,10 +92,10 @@ while(!finalizar){
                 producto = prompt("Ingrese solamente el número del producto a a elegir:\n\n1. Bebida de Coco\n\n2. Bebida de Soya");
                 switch(producto){
                     case "1":
-                        total = total + agregarCarrito(bebidacoco);
+                        total = total + agregarCarrito("bebida de coco");
                         break;
                     case "2":
-                        total = total + agregarCarrito(bebidasoya);
+                        total = total + agregarCarrito("bebida de soya");
                         break;
                     default:
                         alert("Opción inválida, por favor ingrese solo el número de la opción deseada del menú.");
@@ -77,10 +105,10 @@ while(!finalizar){
                 producto = prompt("Ingrese solamente el número del producto a a elegir:\n\n1. Leche Entera\n\n2. Leche Light");
                 switch(producto){
                     case "1":
-                        total = total + agregarCarrito(lecheent);
+                        total = total + agregarCarrito("leche entera");
                         break;
                     case "2":
-                        total = total + agregarCarrito(lechelight);
+                        total = total + agregarCarrito("leche light");
                         break;
                     default:
                         alert("Opción inválida, por favor ingrese solo el número de la opción deseada del menú.");
@@ -90,10 +118,10 @@ while(!finalizar){
                 producto = prompt("Ingrese solamente el número del producto a a elegir:\n\n1. Chocolate amargo (barra 100g)\n\n2. Nuez de la India (500g)");
                 switch(producto){
                     case "1":
-                        total = total + agregarCarrito(chocolate);
+                        total = total + agregarCarrito("chocolate");
                         break;
                     case "2":
-                        total = total + agregarCarrito(nueces);
+                        total = total + agregarCarrito("nuez de la india");
                         break;
                     default:
                         alert("Opción inválida, por favor ingrese solo el número de la opción deseada del menú.");
@@ -117,13 +145,13 @@ while(!finalizar){
 
 if(!cancelled){
     if(login){
-        if(confirm(`¡Gracias por tu compra, ${usuario.name}!\n\nTu total es de \$${total} MXN, y cuentas con un descuento del 15%.\n\nEn total deberás pagar \$${total-discount(login, total)} MXN.\n\nSe te hará llegar tu pedido a ${usuario.direccion}. ¿Deseas continuar?`)){
-            alert("¡Gracias y vuelva pronto!");
+        if(confirm(`¡Gracias por tu compra, ${usuario.name}!\n\nTu total es de \$${total} MXN, y cuentas con un descuento del 15%.\n\nEn total deberás pagar \$${total-discount(login, total)} MXN.\n\nSe te hará llegar tu pedido a ${usuario.direccion}.\n\nTu pedido incluye ${carrito.join(', ')}\n\n¿Deseas continuar?`)){
+            alert(`Su pedido llegará el ${arriving.toLocaleDateString()}.\n\n¡Gracias y vuelva pronto!`);
         }
     }else{
         let ubicacion = prompt(`¡Gracias por tu compra!\n\nTu total es de \$${total} MXN.\n\nPor favor ingresa tu dirección para enviar tu pedido.`);
-        if(confirm(`La dirección es ${ubicacion}.\n\nEl total es de \$${total} MXN. ¿Deseas continuar?`)){
-            alert("¡Gracias y vuelva pronto!");
+        if(confirm(`La dirección es ${ubicacion}.\n\nEl total es de \$${total} MXN.\n\nTu pedido incluye ${carrito.join(', ')}\n\n¿Deseas continuar?`)){
+            alert(`Su pedido llegará el ${arriving.toLocaleDateString()}.\n\n¡Gracias y vuelva pronto!`);
         }
     }
 }
@@ -135,17 +163,8 @@ function NewUser(name, password, direccion){
     this.direccion = direccion;
 }
 
-function agregarCarrito(product){
-    if(product.available){
-        if(confirm(`Este producto tiene un costo de \$${product.price} MXN, ¿seguro que desea agregarlo al carrito?`)){
-            return parseFloat(product.price);
-        }else{
-            return 0
-        }
-    }else{
-        alert(`Lo sentimos, por el momento no contamos con ${product.nombre}.`);
-        return 0;
-    }
+function agregarCarrito(nameprod){
+    return productos[productos.map(function(x){ return x.product.toLowerCase()}).indexOf(nameprod.toLowerCase())].addOrder();
 }
 
 function discount(aplicable, total){
