53f9d9ca4f4378fc559284800700f0199c6e999e
guille-flores <memo.rfl97@gmail.com>
2022-04-21 22:29:50 -0500

    2022-04-21: JSON and LocalStorage implementation

--- javascript/checkout.js
+++ javascript/checkout.js
@@ -1,6 +1,53 @@
-let mainpage = document.getElementById("mainpage__checkout")
-let h1 = document.createElement("h1")
-h1.innerHTML = "Revisa tus productos"
-mainpage.append(h1)
-console.log(mainpage)
+const hoy = new Date();
+const arriving = new Date();
+arriving.setDate(arriving.getDate()+7);
+let carrito = JSON.parse(localStorage.getItem("carrito")); //retrieving the shopping car
+let productos = JSON.parse(localStorage.getItem("productos")) //retrieving the products we have 
+crearProductoCarrito()
+var total = 0;
 
+function crearProductoCarrito(){
+    let tabla = document.getElementById("tcarrito");
+    tabla.innerHTML = "";
+    total = 0
+    for (let ii=0; ii<carrito.length; ii++){ //to modify the rows of the products the user has in their car to show in HTML
+        let articulo = carrito[ii].producto;
+        let precio = carrito[ii].precio;
+        let cantidad = carrito[ii].cantidad;
+        let totalArticulo = parseInt(cantidad)*parseFloat(precio);
+        tabla.innerHTML += 
+        `
+        <tr id="${ii}">
+            <td>${articulo}</td>
+            <td>$${precio}</td>
+            <td>${cantidad}</td>
+            <td>$${totalArticulo}</td>
+            <td><button class="btn btn-danger" onClick="eliminarArticulo(${ii})">Eliminar</button></td>
+        </tr>
+        `;
+        total += totalArticulo;
+    }
+
+    let footercarrito = document.getElementById("carritototal")
+    footercarrito.innerHTML = 
+    `
+    <tr>
+        <td class="right" colspan="3">Total:</td><td class="right">$${total}</td>
+    </tr>
+    `;
+    localStorage.setItem("carrito", JSON.stringify(carrito)); //updating the shopping car
+}
+
+function eliminarArticulo(ii){
+    let articulocarrito = document.getElementById(`${ii}`);
+    for (let ii=0; ii<productos.length; ii++){
+        if (productos[ii].product == articulocarrito.getElementsByTagName("td")[0].innerText){ //We will find the product and increment the stock as we will delete it from the car
+            productos[ii].stock += 1;
+            total -= productos[ii].price*parseFloat(articulocarrito.getElementsByTagName("td")[2].innerText);
+        }
+    }
+    articulocarrito.innerHTML = ""; //we make the table blank
+    carrito.splice(ii,1) //removing the element from the array so it doesn't get written again
+    crearProductoCarrito();
+    localStorage.setItem("carrito", JSON.stringify(carrito)); //updating the shopping car
+}
--- javascript/general.js
+++ javascript/general.js
@@ -1,16 +1,13 @@
-let user = prompt("¡Bienvenido a Simple Nature!\n\nIngrese su nombre: ");
-const carrito = [];
-const hoy = new Date();
-const arriving = new Date();
-arriving.setDate(arriving.getDate()+7);
-var mainpage = document.getElementById("mainpage__general");
+if(sessionStorage.getItem("usuario") == "null"){
+    let user = prompt("¡Bienvenido a Simple Nature!\n\nIngrese su nombre: ");
+    sessionStorage.setItem('usuario', user);
+}
 
-let h2 = document.createElement("h2");
-h2.innerHTML = `Hola ${user}, por favor selecciona tus productos.`;
-mainpage.prepend(h2);
-let h1 = document.createElement("h1");
-h1.innerHTML = `Su carrito tiene los siguientes productos:`;
-mainpage.append(h1)
+if(localStorage.getItem("carrito").length>0){
+    var carrito = JSON.parse(localStorage.getItem("carrito")); //obtaining the shopping car in case the user edited it so we can work over it
+}else{
+    var carrito = [];
+}
 
 //Definiendo las clases que son los productos
 class NewProduct{
@@ -25,13 +22,20 @@ class NewProduct{
         if (this.stock > 0){
             if(confirm(`Este producto tiene un costo de \$${this.price} MXN, ¿seguro que desea agregarlo al carrito?`)){
                 this.stock = this.stock - 1;
-                if (!carrito.includes(this.product)){
-                    carrito.push(this.product);
-                    alert(`${this.product} se ha agregado al carrito, tu total es de \$${total+this.price}\n\nSerá entregado en ${arriving}`);
-                    let li = document.createElement("li");
-                    li.innerHTML = this.product;
-                    mainpage.append(li);
+                if (carrito.filter(x => x.producto == this.product).length <= 0){
+                    const toadd = {
+                        producto: this.product,
+                        precio: this.price,
+                        cantidad: 1
+                    }
+                    carrito.push(toadd);
+                }else{
+                    let posicion = carrito.map(x => x.producto).indexOf(this.product); //we will identify the repeated item so we can change the quantity
+                    carrito[posicion].cantidad += 1;
                 }
+                total += this.price;
+                localStorage.setItem("carrito", JSON.stringify(carrito)); //creating an HTML element to add in a table
+                alert(`${this.product} se ha agregado al carrito`);
                 return parseFloat(this.price);
             }else{
                 return 0;
@@ -58,27 +62,28 @@ productos.push(new NewProduct("Bebida de Almendras", 25,"agua_almendras", 4));
 productos.push(new NewProduct("Bebida de Almendras sin Azúcar", 30, "agua_almendras_sa", 10));
 productos.push(new NewProduct( "Alimento Líquido de Macadamia", 18, "agua_macadamia", 2));
 productos.push(new NewProduct("Jugo de Manzana", 80, "jugo_manzana", 5));
+localStorage.setItem("productos", JSON.stringify(productos));
 
 var botones = [];
 for (let ii=0; ii<productos.length; ii++){
     botones[ii] = document.getElementById(productos[ii].id);
 }
 
-botones[0].onclick = () => {total = total + agregarCarrito(productos[0].id)}; 
-botones[1].onclick = () => {total = total + agregarCarrito(productos[1].id)}; 
-botones[2].onclick = () => {total = total + agregarCarrito(productos[2].id)}; 
-botones[3].onclick = () => {total = total + agregarCarrito(productos[3].id)}; 
-botones[4].onclick = () => {total = total + agregarCarrito(productos[4].id)}; 
-botones[5].onclick = () => {total = total + agregarCarrito(productos[5].id)}; 
-botones[6].onclick = () => {total = total + agregarCarrito(productos[6].id)}; 
-botones[7].onclick = () => {total = total + agregarCarrito(productos[7].id)}; 
-botones[8].onclick = () => {total = total + agregarCarrito(productos[8].id)}; 
-botones[9].onclick = () => {total = total + agregarCarrito(productos[9].id)}; 
-botones[10].onclick = () => {total = total + agregarCarrito(productos[10].id)}; 
-botones[11].onclick = () => {total = total + agregarCarrito(productos[11].id)}; 
-botones[12].onclick = () => {total = total + agregarCarrito(productos[12].id)}; 
+botones[0].onclick = () => {agregarCarrito(productos[0].id)}; 
+botones[1].onclick = () => {agregarCarrito(productos[1].id)}; 
+botones[2].onclick = () => {agregarCarrito(productos[2].id)}; 
+botones[3].onclick = () => {agregarCarrito(productos[3].id)}; 
+botones[4].onclick = () => {agregarCarrito(productos[4].id)}; 
+botones[5].onclick = () => {agregarCarrito(productos[5].id)}; 
+botones[6].onclick = () => {agregarCarrito(productos[6].id)}; 
+botones[7].onclick = () => {agregarCarrito(productos[7].id)}; 
+botones[8].onclick = () => {agregarCarrito(productos[8].id)}; 
+botones[9].onclick = () => {agregarCarrito(productos[9].id)}; 
+botones[10].onclick = () => {agregarCarrito(productos[10].id)}; 
+botones[11].onclick = () => {agregarCarrito(productos[11].id)}; 
+botones[12].onclick = () => {agregarCarrito(productos[12].id)}; 
 
 function agregarCarrito(id){
-    return productos[productos.map(function(x){return x.id}).indexOf(id)].addOrder();
+    productos[productos.map(function(x){return x.id}).indexOf(id)].addOrder();
 }
 
